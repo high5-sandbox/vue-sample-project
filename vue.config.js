36d55a7c44e79dd434f401f5f76e0b956f8bde2c
high5 <high525jp@gmail.com>
2018-09-30 16:52:23 +0900

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
+  outputDir: './main/resources/',
+  assetsDir: './main/assets/',
+  filenameHashing: false,
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/index/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    subpage: {
+      // entry for the page
+      entry: 'src/subpage/main.js',
+      // the source template
+      template: 'public/subpage.html',
+      // output as dist/index.html
+      filename: 'subpage.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Subpage Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'subpage']
+    },
+    // when using the entry-only string format,
+    // template is inferred to be `public/subpage.html`
+    // and falls back to `public/index.html` if not found.
+    // Output filename is inferred to be `subpage.html`.
+    // subpage: 'src/subpage/main.js'
+  }
+}
